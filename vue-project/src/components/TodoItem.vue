1b3bd6aa5a61ac2241caddd65f65e45458175d18
Taivo Türnpu <taivo@ringit.ee>
2024-09-02 09:37:03 +0300

    added new vuejs project

---
+++ vue-project/src/components/TodoItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+<li>{{ msg }}</li>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/stores/counter.js
@@ -0,0 +1,14 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', () => {
+
+  let items = ref(['Learn 1', 'Learn 2', 'Learn 3'])
+  const itemsCount = computed(() => items.value.length)
+
+  function addTodo(todoItems) {
+    //todo: your code
+  }
+
+  return { itemsCount, items, addTodo }
+})
